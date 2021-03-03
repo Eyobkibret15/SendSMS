8b8ffd449e34051e17e8a15dae46bee1a1c5ffec
eyob <ayaleweyob15@gmail.com>
2021-03-03 16:08:41 +0100

    solution code for the task

---
+++ main.py
@@ -0,0 +1,14 @@
+from twilio.rest import Client
+
+# Your Account Sid and Auth Token from twilio.com/console
+account_sid = 'ACcefabb392661ef869a9ca3745f3fe637'
+auth_token = 'dc9e2f84b18907c2c8fd6e61907a97bb'
+client = Client(account_sid, auth_token)
+
+message = client.messages \
+                .create(
+                     body="Join Earth's mightiest heroes. Like eyob kibret.",
+                     from_='+15022731170', # this one is twilio number
+                     to='+48739621422'   # this my real phone number
+                 )
+print(message.sid)
